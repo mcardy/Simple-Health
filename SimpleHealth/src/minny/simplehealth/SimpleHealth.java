b8126d42f1f3f8b8e54abd2b2d2e728bb0170542
Michael <cmichael40@yahoo.com>
2013-04-12 23:38:03 -0400

    Efficiency IV
    Made the code wayyyy more efficient

--- SimpleHealth/src/minny/simplehealth/SimpleHealth.java
+++ SimpleHealth/src/minny/simplehealth/SimpleHealth.java
@@ -1,23 +1,37 @@
 package minny.simplehealth;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
 public class SimpleHealth extends JavaPlugin {
 
-	private final SimpleHealthPlayerListener playerListener = new SimpleHealthPlayerListener();
-	
+	private final SimpleHealthPlayerListener playerListener = new SimpleHealthPlayerListener(
+			this);
+	ScoreboardManager manager = Bukkit.getScoreboardManager();
+	Scoreboard board = manager.getNewScoreboard();
+
 	@Override
 	public void onDisable() {
 	}
+
 	@Override
 	public void onEnable() {
-		
+
+		board.registerNewObjective("showhealth", "health");
+
+		Objective objective = board.getObjective("showhealth");
+		objective.setDisplaySlot(DisplaySlot.BELOW_NAME);
+		objective.setDisplayName("/ 20");
+
 		PluginManager pm = getServer().getPluginManager();
 
 		pm.registerEvents(playerListener, this);
-		
+
 	}
-	
+
 }
--- SimpleHealth/src/minny/simplehealth/SimpleHealthPlayerListener.java
+++ SimpleHealth/src/minny/simplehealth/SimpleHealthPlayerListener.java
@@ -5,29 +5,25 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
 
 public class SimpleHealthPlayerListener implements Listener {
 
+	SimpleHealth plugin;
+
+	public SimpleHealthPlayerListener(SimpleHealth plugin) {
+		this.plugin = plugin;
+	}
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
-		ScoreboardManager manager = Bukkit.getScoreboardManager();
-		Scoreboard board = manager.getNewScoreboard();
-		board.registerNewObjective("showhealth", "health");
-
-		Objective objective = board.getObjective("showhealth");
-		objective.setDisplaySlot(DisplaySlot.BELOW_NAME);
-		objective.setDisplayName("/ 20");
-
-		for (Player online : Bukkit.getOnlinePlayers()) {
-			if (!online.hasPermission("simplehealth.hidehealth")) {
-				if (!online.hasPlayedBefore()){
-					online.setHealth(20);
-				}
-				online.setScoreboard(board);
+		updateScoreboard();
+	}
+	
+	public void updateScoreboard(){
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			if (!p.hasPermission("simplehealth.hidehealth")) {
+				p.setScoreboard(plugin.board);
+				p.setHealth(p.getHealth());
 			}
 		}
 	}
