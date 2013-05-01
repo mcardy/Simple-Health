e118a50c586fe0bd556e3dd9434bf544ab0c1bcc
Michael <cmichael40@yahoo.com>
2013-04-30 21:42:32 -0400

    Merged to one class. Code cleanup now super simple!

--- SimpleHealth/src/minny/simplehealth/SimpleHealth.java
+++ SimpleHealth/src/minny/simplehealth/SimpleHealth.java
@@ -1,43 +1,31 @@
 package minny.simplehealth;
-
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
-
-public class SimpleHealth extends JavaPlugin {
-
-	private final SimpleHealthPlayerListener playerListener = new SimpleHealthPlayerListener(
-			this);
-	
-
+public class SimpleHealth extends JavaPlugin implements Listener{
 	Scoreboard board;
-	
-	@Override
-	public void onDisable() {
-	}
-
 	@Override
 	public void onEnable() {
-		board = getScoreboard(board);
-		
+		board = Bukkit.getScoreboardManager().getNewScoreboard();
 		board.registerNewObjective("showhealth", "health");
-
 		Objective objective = board.getObjective("showhealth");
 		objective.setDisplaySlot(DisplaySlot.BELOW_NAME);
 		objective.setDisplayName("/ 20");
-
-		PluginManager pm = getServer().getPluginManager();
-
-		pm.registerEvents(playerListener, this);
-
+		getServer().getPluginManager().registerEvents(this, this);
 	}
-	
-	public Scoreboard getScoreboard(Scoreboard board){
-		return
-		board = Bukkit.getScoreboardManager().getNewScoreboard();
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			if (!p.hasPermission("simplehealth.hidehealth")) {
+				p.setScoreboard(board);
+				p.setHealth(p.getHealth());
+			}
+		}
 	}
-
 }
